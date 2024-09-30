545d3f3b84836f42224a815f686818e2a7bb0c6a
Diaz <danieldiazarrieta54@gmail.com>
2024-09-30 16:30:08 -0500

    chore: Update .gitignore to ignore additional folders

--- MySQL_Script.sql
+++ MySQL_Script.sql
@@ -1,8 +1,15 @@
--- Create the table for movies
+
 CREATE TABLE movies (
-    id INT AUTO_INCREMENT PRIMARY KEY,
     popularity DECIMAL(10,3),
-    release_date DATE,
+    release_date VARCHAR(255),
     title VARCHAR(255),
     vote_average DECIMAL(5,3)
 );
+
+DROP TABLE IF EXISTS movies;
+
+COPY movies (popularity, release_date, title, vote_average) FROM 'C:\Users\danie\Desktop\Top_Rated_Movies.csv' DELIMITER ',' CSV HEADER;
+
+SELECT * FROM movies;
+
+DROP TABLE movies;
