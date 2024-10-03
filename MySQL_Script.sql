4574d034f03709631236295a976a2027a35fd44f
Mateus Trejos <mateus.lanzoni@eia.edu.co>
2024-10-03 17:06:08 -0500

    chore: Update MySQL_Script.sql and requirements.txt

--- MySQL_Script.sql
+++ MySQL_Script.sql
@@ -8,8 +8,23 @@ CREATE TABLE movies (
 
 DROP TABLE IF EXISTS movies;
 
-COPY movies (popularity, release_date, title, vote_average) FROM '/home/daniel/FastAPI/Top_Rated_Movies.csv' DELIMITER ',' CSV HEADER;
+COPY movies (popularity, release_date, title, vote_average) FROM './Top_Rated_Movies.csv' DELIMITER ',' CSV HEADER;
 
 SELECT * FROM movies;
 
 DROP TABLE movies;
+
+#Debes correr este coigo en la terminal para poder acceder a la base de datos, utilizanddo lo instalado en requirements.txt.
+
+#sudo -i -u postgres
+#psql
+
+#1: CREATE USER postgres WITH PASSWORD 'dollarfen54'; estos son los datos utilizados si se cambian se debe cambiar en el archivo main.py
+#2: CREATE DATABASE sisoptaller owner postgres;
+#3: ALTER USER postgres WITH SUPERUSER;
+#4: psql -U postgres -d aisoptaller
+#5: psql -U miusuario conexion atras de socket local
+
+#cuando termines estos pasos ya deberias tener acceso a la base de datos y poder correr el codigo para crear tabla, luego se puede testear con el select.
+
+
