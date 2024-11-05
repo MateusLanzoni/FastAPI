ad95cb1294371860a37b30d9a283563e3e0c9e3a
Mateus Lanzoni Trejos <mateus.lanzoni@eia.edu.co>
2024-11-04 19:53:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
+import json
+from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException, Query, status
 from pydantic import BaseModel, Field
 from typing import List, Optional, Dict, Any
 from datetime import datetime, date
 import psycopg2
+import boto3
 from psycopg2.extras import RealDictCursor
 import os
 
-db_host = 'localhost'
-db_name = 'sisoptaller'
-db_user = 'sistemas'
-db_password = 'dollarfen54'
+# Cargar variables de entorno desde el archivo .env
+load_dotenv()
+
+# Acceder a las variables de entorno
+db_host = os.getenv('DB_HOST')
+db_name = os.getenv('DB_NAME')
+db_user = os.getenv('DB_USER')
+db_password = os.getenv('DB_PASSWORD')
+aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+aws_region = os.getenv('AWS_REGION')
+aws_s3_bucket = os.getenv('AWS_S3_BUCKET')
+
 
 DATABASE_URL = f"postgresql://{db_user}:{db_password}@{db_host}:5432/{db_name}"
 
@@ -28,6 +40,14 @@ def connect_to_db():
         return conn
     except psycopg2.OperationalError as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Database connection failed")
+    
+
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+    region_name=os.getenv('AWS_REGION')
+)
 
 
 @app.get("/")
@@ -104,6 +124,21 @@ def create_movie(movies: List[Movie]):
                     INSERT INTO movies (popularity, release_date, title, vote_average)
                     VALUES (%s, %s, %s, %s)
                 """, (movie.popularity, movie.release_date, movie.title, movie.vote_average))
+
+                movie_data = {
+                    "popularity": movie.popularity,
+                    "release_date": movie.release_date.isoformat(),
+                    "title": movie.title,
+                    "vote_average": movie.vote_average
+                }
+                
+                s3_client.put_object(
+                    Bucket=os.getenv('AWS_S3_BUCKET'),
+                    Key=f"movies/{movie.title}.json",
+                    Body=json.dumps(movie_data),
+                    ContentType='application/json'
+                )
+
                 registros_agregados += 1
             except Exception as e:
                 print(f"Error al insertar la pelicula {movie.title}: {e}")
